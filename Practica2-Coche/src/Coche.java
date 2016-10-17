d25043b686ac60b94191e2914fc0f5b452ba3574
Markel <markel.alvarez@opendeusto.es>
2016-10-17 20:21:31 +0200

    Commit con el paso 4 casi acabado.

--- Practica2-Coche/src/Coche.java
+++ Practica2-Coche/src/Coche.java
@@ -1,36 +1,47 @@
 
-
-/** Clase para definir instancias lógicas de coches con posición, dirección y velocidad.
- * @author Andoni Eguíluz
- * Facultad de Ingeniería - Universidad de Deusto (2014)
+/**
+ * Clase para definir instancias lógicas de coches con posición, dirección y
+ * velocidad.
+ * 
+ * @author Andoni Eguíluz Facultad de Ingeniería - Universidad de Deusto (2014)
  */
 public class Coche {
-	protected double miVelocidad;  // Velocidad en pixels/segundo
-	protected double miDireccionActual;  // Dirección en la que estoy mirando en grados (de 0 a 360)
-	protected double posX;  // Posición en X (horizontal)
-	protected double posY;  // Posición en Y (vertical)
-	protected String piloto;  // Nombre de piloto
-	
+	protected double miVelocidad; // Velocidad en pixels/segundo
+	protected double miDireccionActual; // Dirección en la que estoy mirando en
+										// grados (de 0 a 360)
+	protected double posX; // Posición en X (horizontal)
+	protected double posY; // Posición en Y (vertical)
+	protected String piloto; // Nombre de piloto
+	public static final double MASA = 1.0D;
+	public static final double FUERZA_BASE_ADELANTE = 2000.0D;
+	public static final double FUERZA_BASE_ATRAS = 1000.0D;
+	public static final double COEF_RZTO_SUELO = 15.5D;
+	public static final double COEF_RZTO_AIRE = 0.35D;
+
 	// Constructores
-	
+
 	public Coche() {
 		miVelocidad = 0;
 		miDireccionActual = 0;
 		posX = 300;
 		posY = 300;
 	}
-	
-	/** Devuelve la velocidad actual del coche en píxeles por segundo
-	 * @return	velocidad
+
+	/**
+	 * Devuelve la velocidad actual del coche en píxeles por segundo
+	 * 
+	 * @return velocidad
 	 */
 	public double getVelocidad() {
 		return miVelocidad;
 	}
 
-	/** Cambia la velocidad actual del coche
+	/**
+	 * Cambia la velocidad actual del coche
+	 * 
 	 * @param miVelocidad
 	 */
-	public void setVelocidad( double miVelocidad ) {
+	public void setVelocidad(double miVelocidad) {
 		this.miVelocidad = miVelocidad;
 	}
 
@@ -38,9 +49,10 @@ public class Coche {
 		return miDireccionActual;
 	}
 
-	public void setDireccionActual( double dir ) {
+	public void setDireccionActual(double dir) {
 		// if (dir < 0) dir = 360 + dir;
-		if (dir > 360) dir = dir - 360;
+		if (dir > 360)
+			dir = dir - 360;
 		miDireccionActual = dir;
 	}
 
@@ -52,19 +64,19 @@ public class Coche {
 		return posY;
 	}
 
-	public void setPosicion( double posX, double posY ) {
-		setPosX( posX );
-		setPosY( posY );
+	public void setPosicion(double posX, double posY) {
+		setPosX(posX);
+		setPosY(posY);
 	}
-	
-	public void setPosX( double posX ) {
-		this.posX = posX; 
+
+	public void setPosX(double posX) {
+		this.posX = posX;
 	}
-	
-	public void setPosY( double posY ) {
-		this.posY = posY; 
+
+	public void setPosY(double posY) {
+		this.posY = posY;
 	}
-	
+
 	public String getPiloto() {
 		return piloto;
 	}
@@ -73,34 +85,70 @@ public class Coche {
 		this.piloto = piloto;
 	}
 
+	/**
+	 * Cambia la velocidad actual del coche
+	 * 
+	 * @param aceleracion
+	 *            Incremento/decremento de la velocidad en pixels/segundo
+	 * @param tiempo
+	 *            Tiempo transcurrido en segundos
+	 */
+	public void acelera(double aceleracion, double tiempo) {
+		miVelocidad = MundoJuego.calcVelocidadConAceleracion(miVelocidad, aceleracion, tiempo);
+	}
 
-	/** Cambia la velocidad actual del coche
-	 * @param aceleracion	Incremento/decremento de la velocidad en pixels/segundo
-	 * @param tiempo	Tiempo transcurrido en segundos
+	/**
+	 * Cambia la dirección actual del coche
+	 * 
+	 * @param giro
+	 *            Angulo de giro a sumar o restar de la dirección actual, en
+	 *            grados (-180 a +180) Considerando positivo giro antihorario,
+	 *            negativo giro horario
 	 */
-	public void acelera( double aceleracion, double tiempo ) {
-		miVelocidad = MundoJuego.calcVelocidadConAceleracion( miVelocidad, aceleracion, tiempo );
+	public void gira(double giro) {
+		setDireccionActual(miDireccionActual + giro);
+	}
+
+	/**
+	 * Cambia la posición del coche dependiendo de su velocidad y dirección
+	 * 
+	 * @param tiempoDeMovimiento
+	 *            Tiempo transcurrido, en segundos
+	 */
+	public void mueve(double tiempoDeMovimiento) {
+		setPosX(posX + MundoJuego.calcMovtoX(miVelocidad, miDireccionActual, tiempoDeMovimiento));
+		setPosY(posY + MundoJuego.calcMovtoY(miVelocidad, miDireccionActual, tiempoDeMovimiento));
+	}
+
+	public double fuerzaAceleracionAdelante() {
+		if (miVelocidad <= -150)
+			return FUERZA_BASE_ADELANTE;
+		else if (miVelocidad <= 0)
+			return FUERZA_BASE_ADELANTE * (-miVelocidad / 150 * 0.5 + 0.5);
+		else if (miVelocidad <= 250)
+			return FUERZA_BASE_ADELANTE * (miVelocidad / 250 * 0.5 + 0.5);
+		else if (miVelocidad <= 750)
+			return FUERZA_BASE_ADELANTE;
+		else
+			return FUERZA_BASE_ADELANTE * (-(miVelocidad - 1000) / 250);
 	}
 	
-	/** Cambia la dirección actual del coche
-	 * @param giro	Angulo de giro a sumar o restar de la dirección actual, en grados (-180 a +180)
-	 * 				Considerando positivo giro antihorario, negativo giro horario
-	 */
-	public void gira( double giro ) {
-		setDireccionActual( miDireccionActual + giro );
+	public double fuerzaAceleracionAtras() {
+		if (miVelocidad <= -350D)
+			return FUERZA_BASE_ATRAS * ((miVelocidad + 500.0D)/150.0D);
+		else if (miVelocidad <= -200.0D)
+			return FUERZA_BASE_ATRAS;
+		else if (miVelocidad <= 0.0D)
+			return FUERZA_BASE_ATRAS * (miVelocidad / 200.0D * 0.7D + 0.3D);
+		else if (miVelocidad <= 250.0D)
+			return FUERZA_BASE_ATRAS * (miVelocidad / 250.0D * 0.55D + 0.3D);
+		else
+			return 850.0D;
 	}
-	
-	/** Cambia la posición del coche dependiendo de su velocidad y dirección
-	 * @param tiempoDeMovimiento	Tiempo transcurrido, en segundos
-	 */
-	public void mueve( double tiempoDeMovimiento ) {
-		setPosX( posX + MundoJuego.calcMovtoX( miVelocidad, miDireccionActual, tiempoDeMovimiento ) );
-		setPosY( posY + MundoJuego.calcMovtoY( miVelocidad, miDireccionActual, tiempoDeMovimiento ) );
-	}
-	
+
 	@Override
 	public String toString() {
-		return piloto + " (" + posX + "," + posY + ") - " +
-			   "Velocidad: " + miVelocidad + " ## Dirección: " + miDireccionActual; 
+		return piloto + " (" + posX + "," + posY + ") - " + "Velocidad: " + miVelocidad + " ## Dirección: "
+				+ miDireccionActual;
 	}
 }
--- Practica2-Coche/src/MundoJuego.java
+++ Practica2-Coche/src/MundoJuego.java
@@ -107,5 +107,35 @@ public class MundoJuego {
 	public static double calcVelocidadConAceleracion( double vel, double acel, double tiempo ) {
 		return vel + (acel*tiempo);
 	}
+	public static double calcFuerzaRozamiento( double masa, double coefRozSuelo,
+			 double coefRozAire, double vel ) {
+			 double fuerzaRozamientoAire = coefRozAire * (-vel); // En contra del movimiento
+			 double fuerzaRozamientoSuelo = masa * coefRozSuelo * ((vel>0)?(-1):1); // Contra mvto
+			 return fuerzaRozamientoAire + fuerzaRozamientoSuelo;
+			 } 
+	
+	public static double calcAceleracionConFuerza( double fuerza, double masa ) {
+		 // 2ª ley de Newton: F = m*a ---> a = F/m
+		 return fuerza/masa;
+		 } 
+	
+	public static void aplicarFuerza( double fuerza, Coche coche ) {
+		 double fuerzaRozamiento = calcFuerzaRozamiento( Coche.MASA ,
+		 Coche.COEF_RZTO_SUELO, Coche.COEF_RZTO_AIRE, coche.getVelocidad() );
+		 double aceleracion = calcAceleracionConFuerza( fuerza+fuerzaRozamiento, Coche.MASA );
+		 if (fuerza==0) {
+		 // No hay fuerza, solo se aplica el rozamiento
+		 double velAntigua = coche.getVelocidad();
+		 coche.acelera( aceleracion, 0.04 );
+		 if (velAntigua>=0 && coche.getVelocidad()<0
+		 || velAntigua<=0 && coche.getVelocidad()>0) {
+		 coche.setVelocidad(0); // Si se está frenando, se para (no anda al revés)
+		 }
+		 } else {
+		 coche.acelera( aceleracion, 0.04 );
+		 }
+		 
+	
+	}
 	
 }
